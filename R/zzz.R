b1681e75ae16724e2aa63778ecb875061b6ab55c
Roger D. Peng [audrey] <rpeng@jhsph.edu>
2007-12-10 11:38:35 -0500

    Use packageStartupMessage

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,8 @@
+################################################################################
+
 .onAttach <- function(lib, pkg) {
-    dcf <- read.dcf(file.path(lib, pkg, "DESCRIPTION"))
-    msg <- gettextf("%s (%s %s)", dcf[, "Title"], dcf[, "Version"],
-                    dcf[, "Date"])
-    message(paste(strwrap(msg), collapse = "\n"))
+	dcf <- read.dcf(file.path(lib, pkg, "DESCRIPTION"))
+	msg <- gettextf("%s (%s)", dcf[, "Title"], dcf[, "Version"])
+
+	packageStartupMessage(paste(strwrap(msg), collapse = "\n"))
 }
