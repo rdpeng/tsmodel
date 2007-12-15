d5552c15ab6f6572f40bca8f5fa278b99188830a
Roger D. Peng [audrey] <rpeng@jhsph.edu>
2007-12-15 09:59:23 -0500

    tabs --> spaces

--- R/NMMAPS-model.R
+++ R/NMMAPS-model.R
@@ -31,40 +31,40 @@
 fitCity <- function(data, formula, df.Time = 7 * 14, df.AgeTime = 1 * 14,
                     df.Temp = 6, df.Dew = 3, smooth = c("ns", "bs"),
                     control = glm.control(epsilon = 1e-10, maxit = 1000)) {
-	smooth <- match.arg(smooth)
-	dataname <- as.name(substitute(data))
+        smooth <- match.arg(smooth)
+        dataname <- as.name(substitute(data))
 
-	## Parse formula to get cause of death and pollutant terms
-	cause <- all.vars(formula)[1]
-	pollterms <- attr(terms(formula), "term.labels")
-	modelFormula <- setupFormula(cause, pollterms, df.Time, df.AgeTime,
-				     df.Temp, df.Dew, smooth)
+        ## Parse formula to get cause of death and pollutant terms
+        cause <- all.vars(formula)[1]
+        pollterms <- attr(terms(formula), "term.labels")
+        modelFormula <- setupFormula(cause, pollterms, df.Time, df.AgeTime,
+                                     df.Temp, df.Dew, smooth)
 
-	## Fit the model!
-	call <- substitute(glm(modelFormula, family = quasipoisson, data = data,
-			       control = control, na.action = na.exclude),
-			   list(modelFormula = modelFormula, data = dataname,
-				control = substitute(control)))
-	fit <- eval.parent(call)
-	structure(fit, class = c("tsModel", class(fit)), pollterms = pollterms)
+        ## Fit the model!
+        call <- substitute(glm(modelFormula, family = quasipoisson, data = data,
+                               control = control, na.action = na.exclude),
+                           list(modelFormula = modelFormula, data = dataname,
+                                control = substitute(control)))
+        fit <- eval.parent(call)
+        structure(fit, class = c("tsModel", class(fit)), pollterms = pollterms)
 }
 
 setupFormula <- function(cause, pollutant, df.Time, df.AgeTime, df.Temp,
                          df.Dew, smooth) {
-	f <- substitute(~ dow + agecat + smooth(tmpd, df.Temp)
-			+ smooth(rmtmpd, df.Temp) + smooth(dptp, df.Dew)
-			+ smooth(rmdptp, df.Dew) + smooth(time, df.Time)
-			+ I(smooth(time, df.AgeTime) * Age2Ind)
-			+ I(smooth(time, df.AgeTime) * Age3Ind),
-			list(df.Time = df.Time, df.AgeTime = df.AgeTime,
-			     df.Temp = df.Temp, df.Dew = df.Dew,
-			     smooth = as.name(smooth)))
+        f <- substitute(~ dow + agecat + smooth(tmpd, df.Temp)
+                        + smooth(rmtmpd, df.Temp) + smooth(dptp, df.Dew)
+                        + smooth(rmdptp, df.Dew) + smooth(time, df.Time)
+                        + I(smooth(time, df.AgeTime) * Age2Ind)
+                        + I(smooth(time, df.AgeTime) * Age3Ind),
+                        list(df.Time = df.Time, df.AgeTime = df.AgeTime,
+                             df.Temp = df.Temp, df.Dew = df.Dew,
+                             smooth = as.name(smooth)))
 
-	## Tack on the pollutant(s) to the end of the formula
-	rhs <- as.character(f)
-	rhs[-1] <- paste(rhs[-1], paste(pollutant, collapse = "+"), sep = "+")
+        ## Tack on the pollutant(s) to the end of the formula
+        rhs <- as.character(f)
+        rhs[-1] <- paste(rhs[-1], paste(pollutant, collapse = "+"), sep = "+")
 
-	## Add LHS and coerce to `formula'
-	as.formula(paste(cause, paste(rhs, collapse = "")))
+        ## Add LHS and coerce to `formula'
+        as.formula(paste(cause, paste(rhs, collapse = "")))
 }
 
--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
 ################################################################################
 
 .onAttach <- function(lib, pkg) {
-	dcf <- read.dcf(file.path(lib, pkg, "DESCRIPTION"))
-	msg <- gettextf("%s (%s)", dcf[, "Title"], dcf[, "Version"])
+        dcf <- read.dcf(file.path(lib, pkg, "DESCRIPTION"))
+        msg <- gettextf("%s (%s)", dcf[, "Title"], dcf[, "Version"])
 
-	packageStartupMessage(paste(strwrap(msg), collapse = "\n"))
+        packageStartupMessage(paste(strwrap(msg), collapse = "\n"))
 }
